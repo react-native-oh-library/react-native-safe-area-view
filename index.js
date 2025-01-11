a7a5f25a308aad69cc3287f2e1350c4fcc5ae1eb
zhengjinshou <zhengjinshou@huawei.com>
2025-01-11 17:42:18 +0800

    release: @react-native-oh-tpl/react-native-safe-area-view@0.11.1-rc.1

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ class SafeView extends Component {
     const WIDTH = isLandscape ? X_HEIGHT : X_WIDTH;
     const HEIGHT = isLandscape ? X_WIDTH : X_HEIGHT;
 
-    this.view._component.measureInWindow?.((winX, winY, winWidth, winHeight) => {
+    this.view._component?.measureInWindow?.((winX, winY, winWidth, winHeight) => {
       if (!this.view) {
         return;
       }
